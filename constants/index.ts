b6efd6d7f128ad49d97e24e117713dfb1ba15247
andricomauludi <mauludi21@gmail.com>
2024-04-21 11:27:36 +0700

    change backend port to https

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,3 @@
 export const COOKIE_NAME = "DOTSWMS";
 export const MAX_AGE = 60 * 60 * 24 * 30; //days;
-export const BACKEND_PORT = "http://103.175.217.209:3001/";
+export const BACKEND_PORT = "https://backend.dots.co.id/";
