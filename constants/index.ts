eabb46d8c912e796d7d2d8ddef1f2f8e79d497d1
andricomauludi <mauludi21@gmail.com>
2024-09-03 20:47:06 +0700

    add

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
 export const COOKIE_NAME = "DOTSWMS";
 export const MAX_AGE = 60 * 60 * 24 * 30; //days;
-export const BACKEND_PORT = "http://localhost:3001/";
-// export const BACKEND_PORT = "https://backend.dots.co.id/";
+// export const BACKEND_PORT = "http://localhost:3001/";
+export const BACKEND_PORT = "https://backend.dots.co.id/";
