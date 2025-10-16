61077b0b0f9edb5c5cf59199898a0b69d4864d17
unknown <mauludi21@gmail.com>
2025-10-16 12:36:49 +0000

    change app/layout to server side

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,18 @@
-"use client";
 import "./globals.css";
 import "./data-tables-css.css";
 import "./satoshi.css";
-import axios, { AxiosError } from "axios";
-
-import { CookiesProvider } from 'next-client-cookies/server';
-
-interface UserResponse {
-  user: string | null;
-  error: AxiosError | null;
-}
-
+import { CookiesProvider } from "next-client-cookies/server";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}) {  
-
-  return (    
-      <html lang="en">
-        <CookiesProvider>
-          {children}
-        </CookiesProvider>
-      </html>    
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <CookiesProvider>{children}</CookiesProvider>
+      </body>
+    </html>
   );
 }
